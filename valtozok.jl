5586f06a8146ab81db94be558087017cd459371e
Illés Gergő <illes.gergo1@gmail.com>
2024-04-25 15:37:52 +0200

    köztes commmit TODO: minél előbb befejezni

--- valtozok.jl
+++ valtozok.jl
@@ -1,30 +1,48 @@
 include("typedefs.jl")
 
-function setInput()::userinputs
-  Nx = 2048 #2048
-  Nt = 1024 # 1024
+#function setInput()::userinputs
+#  Nx = 2048 #2048
+#  Nt = 1024 # 1024
+#
+#  cry = 4 # GaAs
+#
+#
+#  sigma_t = 1e-12
+#  sigma_x = 4e-3
+#  lambda0 = 10.6e-6
+#  I0 = 100e13
+#  
+#  str_prefix = "/home/illesg/cst/2d-calculations/"
+#  STR = str_prefix * "proba-perf1"
+#
+#  #gamma = deg2rad(22)
+#  gamma = acos(ngp(lambda0, 300, cry) / nTHzo(1.5e12 * 2 * pi, 300, cry))
+#
+#  dz = 1e-6
+#
+#  z_end = 4.0e-3 + dz
+#
+#
+#   x = range(-sigma_x, sigma_x, Nx) * 10
+#
+#   t = range(-sigma_t, sigma_t, Nt) * 20
+#  return userinputs(Nx,Nt,cry,sigma_t,sigma_x,lambda0,I0,STR,gamma,dz,z_end,x,t)
+#end
 
-  cry = 4 # GaAs
+enu
 
-
-  sigma_t = 1e-12
-  sigma_x = 4e-3
-  lambda0 = 10.6e-6
-  I0 = 100e13
-  
-  str_prefix = "/home/illesg/cst/2d-calculations/"
-  STR = str_prefix * "proba-perf1"
-
-  #gamma = deg2rad(22)
-  gamma = acos(ngp(lambda0, 300, cry) / nTHzo(1.5e12 * 2 * pi, 300, cry))
-
-  dz = 1e-6
-
-  z_end = 4.0e-3 + dz
-
-
-   x = range(-sigma_x, sigma_x, Nx) * 10
-
-   t = range(-sigma_t, sigma_t, Nt) * 20
-  return userinputs(Nx,Nt,cry,sigma_t,sigma_x,lambda0,I0,STR,gamma,dz,z_end,x,t)
+@kwdef struct userinputs
+  Nx::Int = 2048
+  Nt::Int = 1024
+  cry::Int = 
+  sigma_t::Float64
+  sigma_x::Float64
+  lambda0::Float64
+  I0::Float64
+  STR::String
+  gamma::Float64
+  dz::Float64
+  z_end::Float64
+  x::Vector
+  t::Vector
 end
